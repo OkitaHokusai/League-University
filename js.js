7eaebf991293533024b88ce4cb1a6cdfd764d5cb
Okita <64444620+OkitaHokusai@users.noreply.github.com>
2020-05-25 08:53:19 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+function shurima(){   
+    document.getElementById("theme1").style.backgroundColor= "#c49208",
+    document.getElementById("theme2").style.backgroundColor= "#d4ba26",
+    document.getElementById("theme3").style.border= "#d4ba26 solid 5px",
+    document.getElementById("bg").style.background=" url(Home/Shurima.jpg)"
+}
+function vazio(){   
+    document.getElementById("theme1").style.backgroundColor= "rgb(150, 100, 190)",
+    document.getElementById("theme2").style.backgroundColor= "slateblue",
+    document.getElementById("theme3").style.border= "slateblue solid 5px",
+    document.getElementById("bg").style.background=" url(Home/voidbg.jpg)"
+}
+function shadow(){   
+    document.getElementById("theme1").style.backgroundColor= "#2c592a",
+    document.getElementById("theme2").style.backgroundColor= "#324532",
+    document.getElementById("theme3").style.border= "#324532 solid 5px",
+    document.getElementById("bg").style.background=" url(Home/sombra.JPG)"
+}
